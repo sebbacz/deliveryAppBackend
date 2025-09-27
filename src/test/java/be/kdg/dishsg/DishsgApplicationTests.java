7975e5ca01540885f3e4ac02fdb116cacbe3cea3
Sebastian Gondek <sebastian.gondek@student.kdg.be>
2025-09-27 21:20:43 +0200

    initial commit

---
+++ src/test/java/be/kdg/dishsg/DishsgApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.dishsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
