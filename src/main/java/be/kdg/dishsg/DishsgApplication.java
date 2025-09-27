d67987a1668a4c19258dde04a2f7c0ba76db30fc
Sebastian Gondek <sebastian.gondek@student.kdg.be>
2025-09-27 21:21:28 +0200

    initial commit

---
+++ src/main/java/be/kdg/dishsg/DishsgApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.dishsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishsgApplication.class, args);
+	}
+
+}
